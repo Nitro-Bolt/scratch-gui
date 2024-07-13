0280da85728a6865cdbe2472734aeb7d05418cef
Mistium <92952823+Mistium@users.noreply.github.com>
2024-07-13 13:02:02 +0100

    Create gay.js

---
+++ src/lib/themes/accent/gay.js
@@ -0,0 +1,29 @@
+const guiColors = {
+    'motion-primary': '#078e70', // dark green
+    'motion-primary-transparent': 'rgba(7, 142, 112, 0.9)', // dark green with transparency
+    'motion-tertiary': '#26cfaa', // medium green
+
+    'looks-secondary': '#98e9c1', // light green
+    'looks-transparent': 'rgba(152, 233, 193, 0.35)', // light green with transparency
+    'looks-light-transparent': 'rgba(152, 233, 193, 0.15)', // very light green with transparency
+    'looks-secondary-dark': 'hsla(145, 50%, 75%, 1)', // light green in HSL
+
+    'extensions-primary': 'hsla(150, 100%, 50%, 1)', // medium green in HSL
+    'extensions-tertiary': 'hsla(220, 58%, 62%, 1)', // blue in HSL
+    'extensions-transparent': 'hsla(150, 100%, 50%, 0.43)', // medium green with transparency in HSL
+    'extensions-light': 'hsla(150, 50%, 75%, 1)', // light green in HSL
+
+    'drop-highlight': '#fbfefc', // white
+
+    'menu-bar-background-image': 'linear-gradient(90deg, rgba(7, 142, 112, 0.75) 0%, rgba(38, 207, 170, 0.75) 16.67%, rgba(152, 233, 193, 0.75) 33.33%, rgba(251, 254, 252, 0.75) 50%, rgba(123, 173, 226, 0.75) 66.67%, rgba(80, 73, 203, 0.75) 83.33%, rgba(63, 32, 130, 0.75) 100%)'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#078e70', // dark green
+    checkboxActiveBorder: '#3f2082' // dark purple
+};
+
+export {
+    guiColors,
+    blockColors
+};
