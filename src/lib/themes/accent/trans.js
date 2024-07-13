0127b480bfa0cdbf9f1672b731aaa5606aaeb6f6
Mistium <92952823+Mistium@users.noreply.github.com>
2024-07-13 12:23:04 +0100

    Create trans.js

---
+++ src/lib/themes/accent/trans.js
@@ -0,0 +1,29 @@
+const guiColors = {
+    'motion-primary': '#55CDFC',
+    'motion-primary-transparent': 'rgba(85, 205, 252, 0.9)',
+    'motion-tertiary': '#F7A8B8',
+
+    'looks-secondary': '#F7A8B8',
+    'looks-transparent': 'rgba(247, 168, 184, 0.35)',
+    'looks-light-transparent': 'rgba(247, 168, 184, 0.15)',
+    'looks-secondary-dark': 'hsla(330, 71%, 72%, 1)',
+
+    'extensions-primary': 'hsla(200, 89%, 66%, 1)', 
+    'extensions-tertiary': 'hsla(200, 89%, 58%, 1)',
+    'extensions-transparent': 'hsla(200, 89%, 66%, 0.43)',
+    'extensions-light': 'hsla(200, 57%, 85%, 1)',
+
+    'drop-highlight': '#FFFFFF',
+
+    'menu-bar-background-image': 'linear-gradient(90deg, rgba(85, 205, 252, 0.75) 0%, rgba(247, 168, 184, 0.75) 50%, rgba(255, 255, 255, 0.75) 100%)'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#55CDFC',
+    checkboxActiveBorder: '#F7A8B8'
+};
+
+export {
+    guiColors,
+    blockColors
+};
