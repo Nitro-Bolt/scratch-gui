7b2a8c6599421af7be0f672b6c4b7dfc2c2f7b5c
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-04-25 14:58:03 +0200

    capital letter mistake

--- src/addons/addons/custom-web-font/userscript.js
+++ src/addons/addons/custom-web-font/userscript.js
@@ -16,14 +16,14 @@ export default async function ({ addon, global, console }) {
         var style = document.createElement("style");
         style.className = "customFont";
         style.innerHTML = `
-            @import url('https://fonts.googleapis.com/css2?family=${Font
+            @import url('https://fonts.googleapis.com/css2?family=${font
                 .replaceAll(")", "")
                 .replaceAll("'", "")
                 .replaceAll("}", "")
                 .replaceAll(" ", "+")}:wght@200;300;400;500;600;700&display=swap');
 
             :not(code) {
-                font-family: '${Font
+                font-family: '${font
                     .replaceAll("\\", "")
                     .replaceAll("'", "")
                     .replaceAll("}", "")}', sans-serif;
