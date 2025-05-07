8e04952f8c2d9fa2a697f5215023c773dfc2d475
Cubester <cubester.spamlegends@gmail.com>
2025-05-06 20:50:49 -0400

    Turning blocks now use the angle field (scratch-gui)

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -33,14 +33,14 @@ const motion = function (isInitialSetup, isStage, targetId, colors) {
         </block>
         <block type="motion_turnright">
             <value name="DEGREES">
-                <shadow type="math_number">
+                <shadow type="math_angle">
                     <field name="NUM">15</field>
                 </shadow>
             </value>
         </block>
         <block type="motion_turnleft">
             <value name="DEGREES">
-                <shadow type="math_number">
+                <shadow type="math_angle">
                     <field name="NUM">15</field>
                 </shadow>
             </value>
