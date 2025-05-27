8dad41b84fb54a498a8f2bf05779acdf090e7851
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-05-27 19:24:53 +0200

    fix the buttons, add exporting, fix duplicating, make selected outline more visible

--- src/addons/addons/multi-sprite-selecting/userscript.js
+++ src/addons/addons/multi-sprite-selecting/userscript.js
@@ -30,13 +30,15 @@ export default async function ({ addon, console, msg }) {
         "selectAll": `<svg width="20" height="20" viewBox="0 0 24 24" fill="white"><path d="M3 3h18v2H3V3zm0 4h18v2H3V7zm0 4h12v2H3v-2zm0 4h18v2H3v-2zm0 4h18v2H3v-2z"/></svg>`,
         "unselectAll": `<?xml version="1.0" encoding="UTF-8"?><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="3" y="3" width="14" height="14" rx="2" stroke="#FFFFFF" stroke-width="2" fill="none"/><line x1="6" y1="6" x2="14" y2="14" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"/><line x1="14" y1="6" x2="6" y2="14" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"/></svg>`,
         "deleteIcon": `<?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>Delete</title><defs><path d="M4.54751641,6.99994966 L15.4523042,6.99994966 C15.7284466,6.99994966 15.9523042,7.22380729 15.9523042,7.49994966 C15.9523042,7.51506367 15.9516189,7.5301699 15.9502504,7.54522183 L15.1651793,16.1801783 C15.0715275,17.2102489 14.207924,17.9989808 13.1736049,17.9990897 L6.82662224,17.9997575 C5.79213514,17.9998663 4.92828345,17.2110677 4.83462539,16.180829 L4.04956981,7.54521753 C4.02456905,7.27020922 4.22724022,7.02700381 4.50224854,7.00200306 C4.51729904,7.00063483 4.53240384,6.99994966 4.54751641,6.99994966 Z M7.33333333,4 L7.88603796,2.34188612 C7.95409498,2.13771505 8.14516441,2 8.36037961,2 L11.6396204,2 C11.8548356,2 12.045905,2.13771505 12.113962,2.34188612 L12.6666667,4 L16.5,4 C16.7761424,4 17,4.22385763 17,4.5 L17,5.5 C17,5.77614237 16.7761424,6 16.5,6 L3.5,6 C3.22385763,6 3,5.77614237 3,5.5 L3,4.5 C3,4.22385763 3.22385763,4 3.5,4 L7.33333333,4 Z M8.38742589,4 L11.6125741,4 L11.2792408,3 L8.72075922,3 L8.38742589,4 Z M10,11.7204812 L11.5952436,10.1252376 C11.7905057,9.92997548 12.1070882,9.92997548 12.3023504,10.1252376 L12.3747624,10.1976496 C12.5700245,10.3929118 12.5700245,10.7094943 12.3747624,10.9047564 L10.7795188,12.5 L12.3747624,14.0952436 C12.5700245,14.2905057 12.5700245,14.6070882 12.3747624,14.8023504 L12.3023504,14.8747624 C12.1070882,15.0700245 11.7905057,15.0700245 11.5952436,14.8747624 L10,13.2795188 L8.40475641,14.8747624 C8.20949427,15.0700245 7.89291178,15.0700245 7.69764963,14.8747624 L7.62523762,14.8023504 C7.42997548,14.6070882 7.42997548,14.2905057 7.62523762,14.0952436 L9.22048121,12.5 L7.62523762,10.9047564 C7.42997548,10.7094943 7.42997548,10.3929118 7.62523762,10.1976496 L7.69764963,10.1252376 C7.89291178,9.92997548 8.20949427,9.92997548 8.40475641,10.1252376 L10,11.7204812 Z" id="path-1"></path></defs><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><mask id="mask-2" fill="white"><use xlink:href="#path-1"></use></mask><g mask="url(#mask-2)" fill="#FFFFFF"><rect x="0" y="0" width="20" height="20"></rect></g></g></svg>`,
-        "duplicateIcon": `<?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><rect x="5" y="5" width="13.125" height="13.125" rx="2.2266" ry="2.2266" style="fill:none;stroke:#ffffff;stroke-linejoin:round;stroke-width:1.25"/><path d="M14.9805,5l.0195-0.9375a2.1875,2.1875,0,0,0-2.1875-2.1875H4.375A2.5,2.5,0,0,0,1.875,5V12.8125A2.1875,2.1875,0,0,0,4.0625,15h0.9375" style="fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.25"/><line x1="11.5625" y1="8.4375" x2="11.5625" y2="14.6875"style="fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.25"/><line x1="14.6875" y1="11.5625" x2="8.4375" y2="11.5625"style="fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.25"/></svg>`
+        "duplicateIcon": `<?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><rect x="5" y="5" width="13.125" height="13.125" rx="2.2266" ry="2.2266" style="fill:none;stroke:#ffffff;stroke-linejoin:round;stroke-width:1.25"/><path d="M14.9805,5l.0195-0.9375a2.1875,2.1875,0,0,0-2.1875-2.1875H4.375A2.5,2.5,0,0,0,1.875,5V12.8125A2.1875,2.1875,0,0,0,4.0625,15h0.9375" style="fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.25"/><line x1="11.5625" y1="8.4375" x2="11.5625" y2="14.6875"style="fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.25"/><line x1="14.6875" y1="11.5625" x2="8.4375" y2="11.5625"style="fill:none;stroke:#ffffff;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.25"/></svg>`,
+        "exportIcon": `<svg height="16" viewBox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg"><path d="m12 14v-4h2v4c0 1.1045695-.8954305 2-2 2h-10c-1.1045695 0-2-.8954305-2-2v-10c0-1.1045695.8954305-2 2-2h2v2h-2v10zm-.7142857-8.5h-1.57142859c-.78571428 0-2.35714285 0-4.71428571 2.35714286 0-3.14285715 2.35714286-5.5 4.71428571-5.5h1.57142859v-2.35714286l4.7142857 3.92857143-4.7142857 3.92857143z" fill="#ffffff"/></svg>`
     }
 
     const selectAllButton = createIconButton("Select All", icons["selectAll"], selectAll);
     const unselectAllButton = createIconButton("Unselect All", icons["unselectAll"], unselectAll);
     const deleteButton = createIconButton("Delete Selected Sprites", icons["deleteIcon"], deleteSelected);
     const duplicateButton = createIconButton("Duplicate Selected Sprites", icons["duplicateIcon"], duplicateSelected);
+    const exportButton = createIconButton("Export Selected Sprites", icons["exportIcon"], exportSelected);
 
     const selectedCountText = document.createElement("div");
     selectedCountText.className = "sa-sprite-selected-count";
@@ -47,6 +49,7 @@ export default async function ({ addon, console, msg }) {
     container.appendChild(unselectAllButton);
     container.appendChild(deleteButton);
     container.appendChild(duplicateButton);
+    container.appendChild(exportButton);
     container.appendChild(selectedCountText);
 
     addon.tab.displayNoneWhileDisabled(container, {
@@ -127,7 +130,7 @@ export default async function ({ addon, console, msg }) {
             spriteWrappers.forEach(wrapper => {
                 const spriteId = wrapper.dataset.spriteId;
                 if (selectedSprites.has(spriteId)) {
-                    wrapper.style.outline = "2px solid blue";
+                    wrapper.style.outline = "8px solid blue";
                 } else {
                     wrapper.style.outline = "";
                 }
@@ -190,12 +193,36 @@ export default async function ({ addon, console, msg }) {
         const sprites = Gui.targets.sprites;
         const selected = [...selectedSprites];
 
-        for (const targetId in sprites) {
-            const sprite = sprites[targetId];
-            if (selected.includes(sprite.id)) {
-                vm.duplicateSprite(sprite.id);
-                console.log(`Duplicated sprite: ${sprite.name}`);
-            }
+        const spritesToDuplicate = Object.values(sprites).filter(sprite => selected.includes(sprite.id));
+
+        for (const sprite of spritesToDuplicate) {
+            vm.duplicateSprite(sprite.id);
+            console.log(`Duplicated sprite: ${sprite.name}`);
+        }
+
+        updateSelectedText();
+        highlightSelected();
+        runIsChecked();
+    }
+
+    function exportSelected() {
+        if (!spritesContainer) return;
+        console.log("Exporting selected sprites");
+        const Gui = ReduxStore.getState().scratchGui;
+        const sprites = Gui.targets.sprites;
+        const selected = [...selectedSprites];
+
+        const spritesToExport = Object.values(sprites).filter(sprite => selected.includes(sprite.id));
+
+        for (const sprite of spritesToExport) {
+            vm.exportSprite(sprite.id)
+                .then(content => {
+                    downloadBlob(`${sprite.name}.pms`, content);
+                    console.log(`Exported sprite: ${sprite.name}`);
+                })
+                .catch(err => {
+                    console.error(`Failed to export ${sprite.name}:`, err);
+                });
         }
         updateSelectedText();
         highlightSelected();
