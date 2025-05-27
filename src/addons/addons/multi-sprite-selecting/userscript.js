81d95c8a917354f60a05d2d4897a9cde7c7bfb39
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-05-27 20:10:26 +0200

    fixes

--- src/addons/addons/multi-sprite-selecting/userscript.js
+++ src/addons/addons/multi-sprite-selecting/userscript.js
@@ -215,7 +215,7 @@ export default async function ({ addon, console, msg }) {
         const spritesToExport = Object.values(sprites).filter(sprite => selected.includes(sprite.id));
 
         // from "src/lib/download-blob.js"
-        downloadBlob((filename, blob) => {
+        function downloadBlob(filename, blob) {
             const downloadLink = document.createElement('a');
             document.body.appendChild(downloadLink);
 
@@ -246,7 +246,7 @@ export default async function ({ addon, console, msg }) {
                 };
                 reader.readAsDataURL(blob);
             }
-        };
+        }
         for (const sprite of spritesToExport) {
             vm.exportSprite(sprite.id)
                 .then(content => {
@@ -256,7 +256,7 @@ export default async function ({ addon, console, msg }) {
                 .catch(err => {
                     console.error(`Failed to export ${sprite.name}:`, err);
                 });
-        })
+        }
         updateSelectedText();
         highlightSelected();
         runIsChecked();
