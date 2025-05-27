1508846c5918ab6b986867d041a443f7a4bb58fd
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-05-27 20:01:58 +0200

    more changes

--- src/addons/addons/multi-sprite-selecting/userscript.js
+++ src/addons/addons/multi-sprite-selecting/userscript.js
@@ -130,7 +130,7 @@ export default async function ({ addon, console, msg }) {
             spriteWrappers.forEach(wrapper => {
                 const spriteId = wrapper.dataset.spriteId;
                 if (selectedSprites.has(spriteId)) {
-                    wrapper.style.outline = "8px solid blue";
+                    wrapper.style.outline = "6px solid blue";
                 } else {
                     wrapper.style.outline = "";
                 }
@@ -214,6 +214,39 @@ export default async function ({ addon, console, msg }) {
 
         const spritesToExport = Object.values(sprites).filter(sprite => selected.includes(sprite.id));
 
+        // from "src/lib/download-blob.js"
+        downloadBlob(filename, blob) => {
+            const downloadLink = document.createElement('a');
+            document.body.appendChild(downloadLink);
+
+            // Use special ms version if available to get it working on Edge.
+            if (navigator.msSaveOrOpenBlob) {
+                navigator.msSaveOrOpenBlob(blob, filename);
+                return;
+            }
+
+            if ('download' in HTMLAnchorElement.prototype) {
+                const url = window.URL.createObjectURL(blob);
+                downloadLink.href = url;
+                downloadLink.download = filename;
+                downloadLink.type = blob.type;
+                downloadLink.click();
+                // remove the link after a timeout to prevent a crash on iOS 13 Safari
+                window.setTimeout(() => {
+                    document.body.removeChild(downloadLink);
+                    window.URL.revokeObjectURL(url);
+                }, 1000);
+            } else {
+                // iOS 12 Safari, open a new page and set href to data-uri
+                let popup = window.open('', '_blank');
+                const reader = new FileReader();
+                reader.onloadend = function () {
+                    popup.location.href = reader.result;
+                    popup = null;
+                };
+                reader.readAsDataURL(blob);
+            }
+        };
         for (const sprite of spritesToExport) {
             vm.exportSprite(sprite.id)
                 .then(content => {
@@ -235,7 +268,7 @@ export default async function ({ addon, console, msg }) {
         const state = ReduxStore.getState();
         const newStageSize = state.scratchGui.stageSize;
 
-        if (previousStageSize === 'small' && newStageSize === 'large') {
+        if (JSON.parse(previousStageSize).stageSize === 'small' && JSON.parse(newStageSize).stageSize === 'large') {
             runIsChecked();
             spriteInfoGroup = spriteInfoRowTertiary.querySelector('[class^="sprite-info_group"]');
             spritesContainer = document.querySelector('[class^="sprite-selector_items-wrapper"]');
