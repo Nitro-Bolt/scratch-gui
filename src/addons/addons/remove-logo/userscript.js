3e29819531eb98c4ad6baf9d1b0299c5338da6b8
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-06-18 15:06:14 +0200

    uh

--- src/addons/addons/remove-logo/userscript.js
+++ src/addons/addons/remove-logo/userscript.js
@@ -1,11 +1,11 @@
 export default async function ({ addon, console, msg }) {
     let logo;
-    async function Logo(toggle) {
-        await addon.tab.waitForElement("img[class^='menu-bar_scratch-logo']", {
-            markAsSeen: true,
-            reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
-            reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
-        });
+    await addon.tab.waitForElement("img[class^='menu-bar_scratch-logo']", {
+        markAsSeen: true,
+        reduxEvents: ["scratch-gui/mode/SET_PLAYER", "fontsLoaded/SET_FONTS_LOADED", "scratch-gui/locales/SELECT_LOCALE"],
+        reduxCondition: (state) => !state.scratchGui.mode.isPlayerOnly,
+    });
+    function Logo(toggle) {
         logo = document.querySelector("img[class^='menu-bar_scratch-logo']");
         if (logo) {
             logo.style.display = toggle ? "none" : ""
@@ -14,15 +14,15 @@ export default async function ({ addon, console, msg }) {
         }
     }
 
-    async function addLogo() {
+    function addLogo() {
         Logo(false)
     }
 
-    async function removeLogo() {
+    function removeLogo() {
         Logo(true)
     }
     
-    addon.settings.addEventListener("change", removeLogo);
+    // addon.settings.addEventListener("change", removeLogo);
     addon.self.addEventListener("disabled", addLogo);
     addon.self.addEventListener("reenabled", removeLogo);
 
