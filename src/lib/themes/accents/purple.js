d0737e9ba7d80bbf12ab378248db591e86ac5b54
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-08-19 22:00:12 +0200

    add b̶l̶o̶c̶k̶ ̶c̶o̶l̶o̶r̶s̶ ̶a̶n̶d̶ accents from turbowarp part 2

---
+++ src/lib/themes/accents/purple.js
@@ -0,0 +1,9 @@
+const guiColors = {
+    'motion-primary': 'hsla(260, 60%, 60%, 1)',
+    'motion-primary-transparent': 'hsla(260, 60%, 60%, 0.9)',
+    'motion-tertiary': 'hsla(260, 42%, 51%, 1)',
+};
+
+export {
+    guiColors as default
+};
---
+++ src/lib/themes/accents/red.js
@@ -0,0 +1,16 @@
+const guiColors = {
+    'motion-primary': '#ff4c4c',
+    'motion-primary-transparent': '#ff4c4ce6',
+    'motion-tertiary': '#cc3333',
+
+    'extensions-primary': 'hsla(10, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
+    'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
+    'extensions-light': 'hsla(10, 57%, 85%, 1)',
+
+    'drop-highlight': '#ff8c8c'
+};
+
+export {
+    guiColors as default
+};
