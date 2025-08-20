ec8c8b3eb0d11ec9eb4331421d6c9bf5229f9e92
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-08-20 18:41:57 +0200

    AAAaAaAa

--- src/lib/themes/themePersistance.js
+++ src/lib/themes/themePersistance.js
@@ -49,7 +49,7 @@ const persistTheme = theme => {
 
     const local = localStorage.getItem(ACCENT_KEY)
     
-    const accent = Theme.set(local).accent;
+    const accent = new Theme().set(local).accent;
     document.body.setAttribute("colorAccent", accent)
 };
 
