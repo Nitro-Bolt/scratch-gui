19de74c4a9fa1ace3aeb6d98cd7640f62e59d45f
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-08-21 11:17:05 +0200

    add blue and stop development of rainbow for now..

--- src/lib/themes/index.js
+++ src/lib/themes/index.js
@@ -27,7 +27,9 @@ const ACCENT_MAP = {
     [ACCENT_MAGENTA]: {
         'motion-primary': 'hsla(289, 100%, 54%, 1)'
     },
-    [ACCENT_RAINBOW]: {}
+    /*[ACCENT_RAINBOW]: {
+        'motion-primary': 'linear-gradient(90deg, rgba(255, 0, 0, 0.75) 0%, rgba(255, 154, 0, 0.75) 10%, rgba(208, 222, 33, 0.75) 20%, rgba(79, 220, 74, 0.75) 30%, rgba(63, 218, 216, 0.75) 40%, rgba(47, 201, 226, 0.75) 50%, rgba(28, 127, 238, 0.75) 60%, rgba(95, 21, 242, 0.75) 70%, rgba(186, 12, 248, 0.75) 80%, rgba(251, 7, 217, 0.75) 90%, rgba(255, 0, 0, 0.75) 100%)'
+    }*/
 };
 
 const ACCENT_DEFAULT = ACCENT_LIME;
