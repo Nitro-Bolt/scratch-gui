7b5bb7198c713d26394b085b7ff2a42fc504f8e1
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-09-21 00:59:23 +0200

    does this work?

--- src/components/dm-custom-accent-modal/custom-accent-modal.jsx
+++ src/components/dm-custom-accent-modal/custom-accent-modal.jsx
@@ -160,8 +160,6 @@ const CustomAccentModalComponent = function (props) {
     }
 
     const CUSTOM_ACCENTS_ARRAY = JSON.parse(localStorage.getItem(CUSTOM_ACCENTS_KEY)) == 1 ? [] : JSON.parse(localStorage.getItem(CUSTOM_ACCENTS_KEY))
-    console.log(CUSTOM_ACCENTS_ARRAY)
-    console.log(CUSTOM_ACCENTS_ARRAY == [] ? [{ nothing: true }] : CUSTOM_ACCENTS_ARRAY)
 
     for (const accentData of (CUSTOM_ACCENTS_ARRAY == [] ? [{ nothing: true }] : CUSTOM_ACCENTS_ARRAY)) {
         if (accentData.nothing) continue;
