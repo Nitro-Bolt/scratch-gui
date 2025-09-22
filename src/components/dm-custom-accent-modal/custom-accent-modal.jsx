c0fa22b37ad332ce3dbf864f468cac9feeae7738
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-09-22 18:54:44 +0200

    test

--- src/components/dm-custom-accent-modal/custom-accent-modal.jsx
+++ src/components/dm-custom-accent-modal/custom-accent-modal.jsx
@@ -95,30 +95,26 @@ const CustomAccentComponent = props => {
     const getStateData = () => {
         return [isEnabled, setEnabled, upForDeletion, setUpForDeletion]
     }
-    
-    (async (tickEvent) => {
-        const wait = async (ms) => {await new Promise(r => setTimeout(r, ms))}
-        while (true) {
-            document.body.dispatchEvent(tickEvent);
-            await wait(200)
-        }
-        document.body.addEventListener("TickEvent", (e) => {
-            let isEnabled = getStateData()[0]
-            let setEnabled = getStateData()[1]
-            let upForDeletion = getStateData()[2]
-            if (!upForDeletion) {
-                let NEW_CUSTOM_ACCENTS_ARRAY = (CUSTOM_ACCENTS_ARRAY == [] ? [{ nothing: true }] : CUSTOM_ACCENTS_ARRAY)
-                for (const accentData of NEW_CUSTOM_ACCENTS_ARRAY) {
-                    if (accentData.nothing) continue;
-                    if (accentData.name == props.name) {
-                        if (accentData.enabled !== isEnabled) {
-                            setEnabled(accentData.enabled)
-                        }
+
+    const tickLoop = function() {
+        let isEnabled = getStateData()[0]
+        let setEnabled = getStateData()[1]
+        let upForDeletion = getStateData()[2]
+        if (!upForDeletion) {
+            let NEW_CUSTOM_ACCENTS_ARRAY = (CUSTOM_ACCENTS_ARRAY == [] ? [{ nothing: true }] : CUSTOM_ACCENTS_ARRAY)
+            for (const accentData of NEW_CUSTOM_ACCENTS_ARRAY) {
+                if (accentData.nothing) continue;
+                if (accentData.name == props.name) {
+                    if (accentData.enabled !== isEnabled) {
+                        setEnabled(accentData.enabled)
                     }
                 }
             }
-        })
-    })(tickEvent)
+        }
+        setTimeout(tickLoop, 200)
+    }
+    tickLoop()
+    
     return (
         <div
             className={classNames(styles.divStretchy, 
