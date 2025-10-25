9b9e6cad1f1088250bbcf5875bdcfc6652faca09
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-10-25 14:56:32 +0200

    test

--- src/addons/addons/move-to-first-last/userscript.js
+++ src/addons/addons/move-to-first-last/userscript.js
@@ -4,7 +4,7 @@ export default async ({ addon, console, msg }) => {
   addon.tab.createEditorContextMenu(
     (ctx) => {
       const target = addon.tab.traps.vm.editingTarget;
-      addon.tab.traps.vm.reorderTarget(ctx.index, 0);
+      addon.tab.traps.vm.reorderTarget(target.order, 0);
       queueMicrotask(() => {
         addon.tab.traps.vm.emitTargetsUpdate();
         addon.tab.traps.vm.runtime.emitProjectChanged();
@@ -16,13 +16,14 @@ export default async ({ addon, console, msg }) => {
       position: "assetContextMenuAfterExport",
       order: 1,
       label: "move to first",
-      condition: (ctx) => ctx.index !== 0,
+      condition: () => target.order !== 0,
     }
   );
   addon.tab.createEditorContextMenu(
     (ctx) => {
+      const targets = addon.tab.traps.vm.runtime.targets;
       const target = addon.tab.traps.vm.editingTarget;
-      addon.tab.traps.vm.reorderTarget(ctx.index, Infinity);
+      addon.tab.traps.vm.reorderTarget(target.order, Infinity);
       queueMicrotask(() => {
         addon.tab.traps.vm.emitTargetsUpdate();
         addon.tab.traps.vm.runtime.emitProjectChanged();
@@ -34,7 +35,7 @@ export default async ({ addon, console, msg }) => {
       position: "assetContextMenuAfterExport",
       order: 2,
       label: "move to last",
-      condition: (ctx) => ctx.index !== ctx.target.parentNode.parentNode.childElementCount - 1,
+      condition: () => target.order !== targets.length - 1,
     }
   );
 };
