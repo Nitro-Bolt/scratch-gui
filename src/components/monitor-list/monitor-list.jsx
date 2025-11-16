8c7145ea05cb79b8e8665430abb24983606710f1
Cubester <cubester.spamlegends@gmail.com>
2025-11-15 22:53:04 -0500

    Rename to `tw-sanitize.js` to prepare for moving this code to TurboWarp

--- src/components/monitor-list/monitor-list.jsx
+++ src/components/monitor-list/monitor-list.jsx
@@ -5,7 +5,7 @@ import Monitor from '../../containers/monitor.jsx';
 import PropTypes from 'prop-types';
 import {OrderedMap} from 'immutable';
 import {stageSizeToTransform} from '../../lib/screen-utils';
-import {sanitizeVariableType} from '../../lib/nb-sanitize.js';
+import {sanitizeVariableType} from '../../lib/tw-sanitize.js';
 
 import styles from './monitor-list.css';
 
