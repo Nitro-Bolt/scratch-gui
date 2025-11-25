33eb7b6d6b1ee64bdf91f5e3330925c768f1c84b
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2025-11-25 08:03:13 +0100

    fix

--- src/components/dm-custom-accent-modal/unsupported-browsers.js
+++ src/components/dm-custom-accent-modal/unsupported-browsers.js
@@ -1,4 +1,4 @@
-const isUnsupported = () => /Firefox|Safari/i.test(navigator.userAgent) || (('userAgentData' in navigator) ? /Opera/i.test(JSON.stringify(navigator.userAgentData.brands)) : true);
+const isUnsupported = () => /Firefox|Safari/i.test(navigator.userAgent) || (('userAgentData' in navigator) ? /Opera/i.test(JSON.stringify(navigator.userAgentData.brands)) : !('showOpenFilePicker' in window));
 
 export {
     isUnsupported,
