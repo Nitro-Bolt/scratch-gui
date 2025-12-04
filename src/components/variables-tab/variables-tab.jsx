6e9a3a8db7013d1861a4f65e50a90e68b5146910
Fath11 <fathimkamal@gmail.com>
2025-12-04 17:14:12 +0800

    Resolved most eslint errors so MY EDITOR CAN STOP SCREAMING AT ME.

--- src/components/variables-tab/variables-tab.jsx
+++ src/components/variables-tab/variables-tab.jsx
@@ -1,192 +1,192 @@
+/* eslint-disable require-jsdoc */
 import React from 'react';
-import styles from './variables-tab.css'
-import { defineMessages, injectIntl } from 'react-intl';
-import dropdownCaret from './dropdown-caret.svg';
+import styles from './variables-tab.css';
+import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 const messages = defineMessages({
-  forThisSprite: {
-    id: 'gui.variableManager.forThisSprite',
-    description: 'Label for sprite-specific variables',
-    defaultMessage: 'For this sprite '
-  },
-  forAllSprites: {
-    id: 'gui.variableManager.forAllSprites',
-    description: 'Label for global variables',
-    defaultMessage: 'For all sprites '
-  },
-  noVariables: {
-    id: 'gui.variableManager.noVariables',
-    description: 'Label when there is no variables of any type',
-    defaultMessage: 'No Variables'
-  },
-  original: {
-    id: 'gui.variableManager.original',
-    description: 'Label for the original sprite.',
-    defaultMessage: 'Original'
-  },
-  clone: {
-    id: 'gui.variableManager.clone',
-    description: 'Label for a clone followed by a space. Will be used like: "Clone 1", "Clone 2", etc...',
-    defaultMessage: 'Clone '
-  }
-})
+    forThisSprite: {
+        id: 'gui.variableManager.forThisSprite',
+        description: 'Label for sprite-specific variables',
+        defaultMessage: 'For this sprite '
+    },
+    forAllSprites: {
+        id: 'gui.variableManager.forAllSprites',
+        description: 'Label for global variables',
+        defaultMessage: 'For all sprites '
+    },
+    noVariables: {
+        id: 'gui.variableManager.noVariables',
+        description: 'Label when there is no variables of any type',
+        defaultMessage: 'No Variables'
+    },
+    original: {
+        id: 'gui.variableManager.original',
+        description: 'Label for the original sprite.',
+        defaultMessage: 'Original'
+    },
+    clone: {
+        id: 'gui.variableManager.clone',
+        description:
+            'Label for a clone followed by a space. Will be used like: "Clone 1", "Clone 2", etc...',
+        defaultMessage: 'Clone '
+    }
+});
 
-function _noVariablesItem(intl) {
-  return (
-    <div className={ styles.box }>
-      { intl.formatMessage(messages.noVariables) }
+/**
+ * @param {Intl} intl For formating.
+ * @returns {HTMLElement} The "No Variables" message.
+ */
+const _noVariablesItem = intl => (
+    <div className={styles.box}>
+        {intl.formatMessage(messages.noVariables)}
     </div>
-  )
-}
+);
 
-function _isList(varr) {
-  return varr.type === 'list';
-}
+const _isList = varr => (varr.type === 'list');
 
-function Variables({variables, intl}) {
-  let final = []
-  for (const id in variables) {
-    const currentVar = variables[id]
-    
-    final.push(
-      <tr key={id}>
-        <td className={styles.variableName}>
-          <input value={currentVar.name}/>
-        </td>
-        <td className={styles.variableValue}>
-          <input value={_isList(currentVar) ? 'List not supported yet' : currentVar.value}/>
-        </td>
-      </tr>
-    )
-  }
+/* eslint-disable react/prop-types */
+const Variables = ({ variables, intl }) => {
+    const final = [];
+    for (const id in variables) {
+        const currentVar = variables[id];
 
-  if (final.length > 0) return (
-    <table>
-      <tbody>{ final }</tbody>
-    </table>
-  )
-  return _noVariablesItem(intl)
-}
+        final.push(
+            <tr key={id}>
+                <td className={styles.variableName}>
+                    <input value={currentVar.name} />
+                </td>
+                <td className={styles.variableValue}>
+                    <input
+                        value={
+                            _isList(currentVar) ? 'List not supported yet' : currentVar.value
+                        }
+                    />
+                </td>
+            </tr>
+        );
+    }
 
-function LocalVariables({clones, handleSpriteHighlighting, intl}) {
-  const [selectedClone, setSelectedClone] = React.useState(0)
-
-  const variables = clones[selectedClone]?.variables
-  return (
-    <div className={styles.localVariablesSelector}>
-
-      <div className={styles.clonesSelectorItemsWrapper}>
-        { clones.map(({ id }, i) => (
-          <div 
-            className={classNames(styles.clonesSelectorItem, {
-              [styles.isSelected]: selectedClone === i
-            })}
-            onClick={() => { 
-              handleSpriteHighlighting(id)
-              setSelectedClone(i)
-            }}
-          >
-            <span className={styles.variableName}>
-              { i === 0 ? intl.formatMessage(messages.original) : intl.formatMessage(messages.clone) +  i} <br/>
-              <sub>{ '(' + id + ')' }</sub>
-            </span>
-          </div>
-        )) }
-      </div>
-
-      {
-        !variables || Object.keys(variables).length > 0
-        ? <div className={styles.localVariablesList}>
-            <Variables 
-              variables={variables}
-              intl={intl}
-            />
-          </div>
-        : _noVariablesItem(intl)
-      }
-
-    </div>
-  )
-}
-
-// The CSS to make the dropdown look good is beyond me
-// Someone please please fix my CSS
-function VariableDropdown({ label, style, children }) {
-  //const [isCollapsed, setIsCollapsed] = React.useState(false)
-
-  return (
-    <div 
-      className={ styles.variableDropdown }
-      style={ style }
-    >
-      <div 
-        className={ styles.variableDropdownHeader }
-        //onClick={ () => setIsCollapsed(isCollapsed => !isCollapsed) }
-      >
-        {/*<img
-          src={dropdownCaret}
-          draggable={false}
-          width={8}
-          height={5}
-          style={{
-            transform: `rotate(${isCollapsed ? -90 : 0}deg)`
-          }}
-        />*/}
-        <span className={styles.variableDropdownHeaderLabel}>{ label }</span>
-      </div>
-
-      { //isCollapsed ? '' :  
-        <div className={ styles.variableDropdownBody }>
-            { children }
-        </div>
-      }
-
-    </div>
-  )
+    if (final.length > 0) {
+        return (
+            <table>
+                <tbody>{final}</tbody>
+            </table>
+        );
+    }
+    return _noVariablesItem(intl);
 };
 
-function VariableTab({
-  intl,
-  isRtl,
-  globalVariables,
-  clones,
-  handleSpriteHighlighting,
-  isStage
-}) {
+/**
+ * @param {Array} param0 List of clones.
+ * @param {Function} param1 Ran once when a clone is selected.
+ * @returns {HTMLElement} Display of every variables in the selected clone.
+ */
+const LocalVariables = ({ clones, onHighlightSprite, intl }) => {
+    const [selectedClone, setSelectedClone] = React.useState(0);
+    const variables = clones[selectedClone]?.variables;
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <VariableDropdown 
-          label={ intl.formatMessage(messages.forAllSprites) }
-          style={{
-            minHeight: `${isStage ? '100' : '30'}%`
-          }}
+    return (
+        <div className={styles.localVariablesSelector}>
+            <div className={styles.clonesSelectorItemsWrapper}>
+                {clones.map(({ id }, i) => (
+                    <div
+                        className={classNames(styles.clonesSelectorItem, {
+                            [styles.isSelected]: selectedClone === i
+                        })}
+                        // eslint-disable-next-line react/jsx-no-bind
+                        onClick={() => {
+                            onHighlightSprite(id);
+                            setSelectedClone(i);
+                        }}
+                        key={id}
+                    >
+                        <span className={styles.variableName}>
+                            {i === 0 ? intl.formatMessage(messages.original) : intl.formatMessage(messages.clone) + i}
+                            {' '}
+                            <br />
+                            <sub>{`(${id})`}</sub>
+                        </span>
+                    </div>
+                ))}
+            </div>
+
+            {!variables || Object.keys(variables).length > 0 ? (
+                <div className={styles.localVariablesList}>
+                    <Variables
+                        variables={variables}
+                        intl={intl}
+                    />
+                </div>
+            ) : (
+                _noVariablesItem(intl)
+            )}
+        </div>
+    );
+};
+
+const VariableDropdown = function ({ label, style, children }) {
+    return (
+        <div
+            className={styles.variableDropdown}
+            style={style}
         >
-          <Variables 
-            variables={globalVariables}
-            intl={intl}
-          />
-        </VariableDropdown>
-        
-        {isStage ? '' : 
-          <VariableDropdown 
-            label={ intl.formatMessage(messages.forThisSprite) }
-            style={{
-              minHeight: '70%'
-            }}
-          >
-            <LocalVariables
-              clones={clones}
-              handleSpriteHighlighting={handleSpriteHighlighting}
-              intl={intl}
-            />
-          </VariableDropdown> 
-        }
-      </div>
+            <div className={styles.variableDropdownHeader} >
+                <span className={styles.variableDropdownHeaderLabel}>
+                    {label}
+                </span>
+            </div>
+
+            {<div className={styles.variableDropdownBody}>{children}</div>}
+        </div>
+    );
+};
+/* eslint-enable react/prop-types */
+
+const VariableTab = props => (
+    <div className={styles.wrapper}>
+        <div className={styles.container}>
+            <VariableDropdown
+                label={props.intl.formatMessage(messages.forAllSprites)}
+                style={{
+                    minHeight: `${props.isStage ? '100' : '30'}%`
+                }}
+            >
+                <Variables
+                    variables={props.globalVariables}
+                    intl={props.intl}
+                />
+            </VariableDropdown>
+
+            {props.isStage ? (
+                ''
+            ) : (
+                <VariableDropdown
+                    label={props.intl.formatMessage(messages.forThisSprite)}
+                    style={{
+                        minHeight: '70%'
+                    }}
+                >
+                    <LocalVariables
+                        clones={props.clones}
+                        selectedClone={props.selectedClone}
+                        onHighlightSprite={props.handleSpriteHighlighting}
+                        intl={props.intl}
+                    />
+                </VariableDropdown>
+            )}
+        </div>
     </div>
-  )
-}
+);
+
+VariableTab.propTypes = {
+    intl: intlShape,
+    isStage: PropTypes.bool,
+    globalVariables: PropTypes.object,
+    clones: PropTypes.array,
+    selectedClone: PropTypes.number,
+    handleSpriteHighlighting: PropTypes.func
+};
 
 export default injectIntl(VariableTab);
--- src/containers/variable-manager.jsx
+++ src/containers/variable-manager.jsx
@@ -9,205 +9,211 @@ import PropTypes from 'prop-types';
 import { highlightTarget } from '../reducers/targets';
 
 class VariableManager extends React.Component {
-  constructor (props) {
-    super(props);
-    
-    bindAll(this, [
-      'reload',
-      '_reloadGlobalVariable',
-      '_reloadLocalVariable',
-      '_handleSpriteHighlighting',
-      '_forceReloadLocalVariable'
-    ]);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      globalVariables: {},
-      clones: []
-    }
+        bindAll(this, [
+            'reload',
+            '_reloadGlobalVariable',
+            '_reloadLocalVariable',
+            '_handleSpriteHighlighting',
+            '_forceReloadLocalVariable'
+        ]);
 
-    /* State format
-    {
-      globalVariable: {
-        [id]: Variable Object,
-        ...
-      },
-      clones: [
+        this.state = {
+            globalVariables: {},
+            clones: []
+        };
+
+        /* State format
         {
-          id: String,
-          variables: {
+          globalVariable: {
             [id]: Variable Object,
             ...
-          }
-        },
-        ...
-      ]
-    }
-    */
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.editingTarget.id !== this.props.editingTarget.id) {
-      this._forceReloadLocalVariable();
-    }
-  }
-
-  componentDidMount() {
-    this.props.vm.runtime.on('RUNTIME_STEP_START', this.reload);
-    this.props.vm.runtime.on('PROJECT_LOADED', this.reload);
-    this.props.vm.runtime.on('TOOLBOX_EXTENSIONS_NEED_UPDATE', this.reload);
-
-    this.reload()
-  }
-
-  componentWillUnmount() {
-    this.props.vm.runtime.off('RUNTIME_STEP_START', this.reload);
-    this.props.vm.runtime.off('PROJECT_LOADED', this.reload);
-    this.props.vm.runtime.off('TOOLBOX_EXTENSIONS_NEED_UPDATE', this.reload);
-  }
-
-  _reloadGlobalVariable() {
-    const stage = this.props.stage
-    if(!stage) return
-    const newVariables = Object.values(stage.variables)
-    const oldVariables = this.state.globalVariables
-
-    for (const id in newVariables) {
-      const varToCheck = oldVariables[id]
-      
-      if (!varToCheck) {
-        return this.setState({
-          globalVariables: structuredClone(stage.variables)
-        })
-      }
-
-      if (varToCheck.name !== newVariables[id].name || varToCheck.value !== newVariables[id].value) {
-        return this.setState({
-          globalVariables: structuredClone(stage.variables)
-        })
-      }
-    }
-  }
-
-  _reloadLocalVariable() {
-    const clones = this.props.sprite.clones.map(({id, variables}, i) => (
-      {
-        id: id,
-        variables: variables
-      }
-    ))
-
-    for (let i = 0; i < clones.length; i++) {
-      const newVariables = clones[i].variables
-      const oldClones = this.state.clones[i]
-      const oldVariables = oldClones?.variables
-
-      if (!oldClones || !oldVariables) {
-        return this.setState({
-            clones: structuredClone(clones)
-          })
-      }
-
-      for (const id in newVariables) {
-        const varToCheck = oldVariables[id]
-        
-        if (!varToCheck) {
-          return this.setState({
-            clones: structuredClone(clones)
-          })
+          },
+          clones: [
+            {
+              id: String,
+              variables: {
+                [id]: Variable Object,
+                ...
+              }
+            },
+            ...
+          ]
         }
+        */
+    }
 
-        if (varToCheck.name !== newVariables[id].name || varToCheck.value !== newVariables[id].value) {
-          return this.setState({
-            clones: structuredClone(clones)
-          })
+    componentDidMount() {
+        this.props.vm.runtime.on('RUNTIME_STEP_START', this.reload);
+        this.props.vm.runtime.on('PROJECT_LOADED', this.reload);
+        this.props.vm.runtime.on('TOOLBOX_EXTENSIONS_NEED_UPDATE', this.reload);
+
+        this.reload();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.editingTarget.id !== this.props.editingTarget.id) {
+            this._forceReloadLocalVariable();
         }
-      }
     }
-  }
 
-  _forceReloadLocalVariable() {
-    const clones = this.props.sprite.clones.map(({id, variables}, i) => (
-      {
-        id: id,
-        variables: variables
-      }
-    ))
-
-    this.setState({
-      clones: structuredClone(clones)
-    })
-  }
-
-  reload() {
-    try {
-      this._reloadGlobalVariable();
-      this._reloadLocalVariable();
-    } catch (e) {
-        console.error(e);
+    componentWillUnmount() {
+        this.props.vm.runtime.off('RUNTIME_STEP_START', this.reload);
+        this.props.vm.runtime.off('PROJECT_LOADED', this.reload);
+        this.props.vm.runtime.off('TOOLBOX_EXTENSIONS_NEED_UPDATE', this.reload);
     }
-  }
 
-  _handleSpriteHighlighting(id) {
-    this.props.dispatchHighlightTarget(id)
-  }
+    _reloadGlobalVariable() {
+        const stage = this.props.stage;
+        if (!stage) return;
+        const newVariables = Object.values(stage.variables);
+        const oldVariables = this.state.globalVariables;
 
-  render () {
-    // The `clones` object needs to be formatted because it have a lot of other useless propeties that should not be copied to State
-    // But on first load, the formating won't run so we need to pass the `clones` object directly
-    // IDK why `globalVariables` aren't affected by this and IDC, it works
-    const isClonesFormatted = this.state.clones.length > 0
-    return (
-      <VariableTab
-        isRtl={this.props.isRtl}
-        globalVariables={this.state.globalVariables}
-        clones={isClonesFormatted ? this.state.clones : this.props.editingTarget.sprite.clones}
-        handleSpriteHighlighting={this._handleSpriteHighlighting}
-        intl={this.props.intl}
-        isStage={this.props.editingTarget.isStage}
-      />
-    )
-  }
+        for (const id in newVariables) {
+            const varToCheck = oldVariables[id];
+
+            if (!varToCheck) {
+                return this.setState({
+                    globalVariables: structuredClone(stage.variables)
+                });
+            }
+
+            if (varToCheck.name !== newVariables[id].name || varToCheck.value !== newVariables[id].value) {
+                return this.setState({
+                    globalVariables: structuredClone(stage.variables)
+                });
+            }
+        }
+    }
+
+    _reloadLocalVariable() {
+        const clones = this.props.sprite.clones.map(({ id, variables }) => (
+            {
+                id: id,
+                variables: variables
+            }
+        ));
+
+        for (let i = 0; i < clones.length; i++) {
+            const newVariables = clones[i].variables;
+            const oldClones = this.state.clones[i];
+            const oldVariables = oldClones?.variables;
+
+            if (!oldClones || !oldVariables) {
+                return this.setState({
+                    clones: structuredClone(clones)
+                });
+            }
+
+            for (const id in newVariables) {
+                const varToCheck = oldVariables[id];
+
+                if (!varToCheck) {
+                    return this.setState({
+                        clones: structuredClone(clones)
+                    });
+                }
+
+                if (varToCheck.name !== newVariables[id].name || varToCheck.value !== newVariables[id].value) {
+                    return this.setState({
+                        clones: structuredClone(clones)
+                    });
+                }
+            }
+        }
+    }
+
+    _forceReloadLocalVariable() {
+        const clones = this.props.sprite.clones.map(({ id, variables }) => (
+            {
+                id: id,
+                variables: variables
+            }
+        ));
+
+        this.setState({
+            clones: structuredClone(clones)
+        });
+    }
+
+    reload() {
+        try {
+            this._reloadGlobalVariable();
+            this._reloadLocalVariable();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    _handleSpriteHighlighting(id) {
+        this.props.dispatchHighlightTarget(id);
+    }
+
+    render() {
+        // The `clones` object needs to be formatted
+        // because it have a lot of other useless propeties that should not be copied to State
+        // But on first load, the formating won't run so we need to pass the `clones` object directly
+        // IDK why `globalVariables` aren't affected by this and IDC, it works
+        const isClonesFormatted = this.state.clones.length > 0;
+        return (
+            <VariableTab
+                isRtl={this.props.isRtl}
+                globalVariables={this.state.globalVariables}
+                clones={isClonesFormatted ? this.state.clones : this.props.editingTarget.sprite.clones}
+                handleSpriteHighlighting={this._handleSpriteHighlighting}
+                intl={this.props.intl}
+                isStage={this.props.editingTarget.isStage}
+            />
+        );
+    }
 }
 
 VariableManager.propTypes = {
-  editingTarget: PropTypes.shape({
-    [PropTypes.string]: PropTypes.shape({
-      id: PropTypes.string,
-      variables: PropTypes.shape({
-        [PropTypes.string]: PropTypes.shape({
-          id: PropTypes.string,
-          value: PropTypes.string
-        }),
-      }),
-    }),
-  }),
-  stage: PropTypes.shape({
-    variables: PropTypes.shape({
-      [PropTypes.string]: PropTypes.shape({
-        id: PropTypes.string,
-        value: PropTypes.string
-      }),
-    }),
-  }),
-  intl: intlShape,
-  isRtl: PropTypes.bool,
-  vm: PropTypes.instanceOf(VM).isRequired,
-}
+    editingTarget: {
+        [PropTypes.string]: {
+            id: PropTypes.string,
+            variables: PropTypes.shape({
+                [PropTypes.string]: PropTypes.shape({
+                    id: PropTypes.string,
+                    value: PropTypes.string
+                })
+            }),
+            isStage: PropTypes.bool
+        }
+    },
+    stage: {
+        variables: {
+            [PropTypes.string]: PropTypes.shape({
+                id: PropTypes.string,
+                value: PropTypes.string
+            })
+        }
+    },
+    intl: intlShape,
+    isRtl: PropTypes.bool,
+    vm: PropTypes.instanceOf(VM).isRequired,
+    sprite: {
+        clones: PropTypes.array
+    },
+    dispatchHighlightTarget: PropTypes.func
+};
 
 const mapStateToProps = state => ({
-  isRtl: state.locales.isRtl,
-  stage: state.scratchGui.targets.stage,
-  editingTarget: state.scratchGui.vm.editingTarget,
-  sprite: state.scratchGui.vm.editingTarget.sprite,
+    isRtl: state.locales.isRtl,
+    stage: state.scratchGui.targets.stage,
+    editingTarget: state.scratchGui.vm.editingTarget,
+    sprite: state.scratchGui.vm.editingTarget.sprite
 });
 
 const mapDispatchToProps = dispatch => ({
-  dispatchHighlightTarget: id => dispatch(highlightTarget(id))
+    dispatchHighlightTarget: id => dispatch(highlightTarget(id))
 });
 
 export default errorBoundaryHOC('Variable Manager')(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(VariableManager)
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(VariableManager)
 );
