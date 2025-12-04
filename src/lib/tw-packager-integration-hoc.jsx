45f11d9abf9a757b99857479efad05aae2c11a84
Cubester <cubester.spamlegends@gmail.com>
2025-12-03 21:35:24 -0500

    Try this

--- src/lib/tw-packager-integration-hoc.jsx
+++ src/lib/tw-packager-integration-hoc.jsx
@@ -5,7 +5,7 @@ import log from './log';
 import {getIsShowingProject} from '../reducers/project-state';
 
 const PACKAGER_URL = 'https://nitro-bolt.github.io/packager';
-const PACKAGER_ORIGIN = PACKAGER_URL;
+const PACKAGER_ORIGIN = 'https://nitro-bolt.github.io';
 
 const PackagerIntegrationHOC = function (WrappedComponent) {
     class PackagerIntegrationComponent extends React.Component {
