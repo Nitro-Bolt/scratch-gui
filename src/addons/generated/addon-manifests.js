3a78d99e0590823a0997386eda66466e977e6f87
Fath11 <fathimkamal@gmail.com>
2026-01-01 10:48:11 +0800

    purged jump-to-def addon

--- src/addons/generated/addon-manifests.js
+++ src/addons/generated/addon-manifests.js
@@ -3,7 +3,7 @@ import _cat_blocks from "../addons/cat-blocks/_manifest_entry.js";
 import _editor_devtools from "../addons/editor-devtools/_manifest_entry.js";
 import _find_bar from "../addons/find-bar/_manifest_entry.js";
 import _middle_click_popup from "../addons/middle-click-popup/_manifest_entry.js";
-import _jump_to_def from "../addons/jump-to-def/_manifest_entry.js";
+//import _jump_to_def from "../addons/jump-to-def/_manifest_entry.js";
 import _reorder_custom_inputs from "../addons/reorder-custom-inputs/_manifest_entry.js";
 import _editor_searchable_dropdowns from "../addons/editor-searchable-dropdowns/_manifest_entry.js";
 import _data_category_tweaks_v2 from "../addons/data-category-tweaks-v2/_manifest_entry.js";
@@ -84,7 +84,7 @@ export default {
   "editor-devtools": _editor_devtools,
   "find-bar": _find_bar,
   "middle-click-popup": _middle_click_popup,
-  "jump-to-def": _jump_to_def,
+  //"jump-to-def": _jump_to_def,
   "reorder-custom-inputs": _reorder_custom_inputs,
   "editor-searchable-dropdowns": _editor_searchable_dropdowns,
   "data-category-tweaks-v2": _data_category_tweaks_v2,
