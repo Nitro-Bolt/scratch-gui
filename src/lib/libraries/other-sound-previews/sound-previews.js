18552b5f216c5c8d49c72390c143dffc5209a65a
Gabs The Mr Incredible Maker <140965985+GabsTheCuriousKid@users.noreply.github.com>
2026-01-05 18:35:56 +0100

    add some sounds

--- src/lib/libraries/other-sound-previews/sound-previews.js
+++ src/lib/libraries/other-sound-previews/sound-previews.js
@@ -1,5 +1,11 @@
 import mibup15full from './mibup15full.png';
+import sonarPing from './sonar-ping.svg';
+import river2 from './river-2.svg';
+import elevatorDing from './elevator-ding.svg';
 
 export default {
-    "mibup15full": mibup15full
+    "mibup15full": mibup15full,
+    "sonar-ping": sonarPing,
+    "river-2": river2,
+    "elevator-ding": elevatorDing
 }
