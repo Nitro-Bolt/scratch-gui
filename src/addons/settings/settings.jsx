91481d121ab91d3ef69d102bbd1ab752c18f3266
raynecloudy <contact@raynec.dev>
2026-02-26 18:08:24 -0800

    fix issue where addon search bar would be filled in with project id

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -917,7 +917,7 @@ class AddonSettingsComponent extends React.Component {
         this.state = {
             loading: false,
             dirty: false,
-            search: getInitialSearch(),
+            search: props.onDirty ? getInitialSearch() : '',
             extended: false,
             ...this.readFullAddonState()
         };
