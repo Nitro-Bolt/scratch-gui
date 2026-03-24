7b95d62d0482b6d357a700c04ec3fd24f42764f4
Cubester <cubester.spamlegends@gmail.com>
2026-03-24 17:34:04 -0400

    Revert "Move away from exposing the VM, but still use some black magic :smile :"
    This reverts commit dbfa87354f98ecafb84d41627f9dd59a2786989e.

--- src/containers/blocks.jsx
+++ src/containers/blocks.jsx
@@ -158,6 +158,9 @@ class Blocks extends React.Component {
         const workspaceConfig = defaultsDeep({},
             this.props.options,
             {
+                vm: this.props.vm
+            },
+            {
                 rtl: this.props.isRtl,
                 toolbox: this.props.toolboxXML,
                 colours: this.props.theme.getBlockColors(),
@@ -168,36 +171,7 @@ class Blocks extends React.Component {
             Blocks.defaultOptions
         );
         this.workspace = this.ScratchBlocks.inject(this.blocks, workspaceConfig);
-
-        if (this.ScratchBlocks.Procedures &&
-            this.ScratchBlocks.Procedures.setProcedureBlocksAcrossTargetsCallback) {
-            this.ScratchBlocks.Procedures.setProcedureBlocksAcrossTargetsCallback(() => {
-                const runtime = this.props.vm && this.props.vm.runtime;
-                if (!runtime || !runtime.targets) {
-                    return [];
-                }
-
-                const procedureBlocks = [];
-                for (let i = 0; i < runtime.targets.length; i++) {
-                    const target = runtime.targets[i];
-                    const blocks = target && target.blocks && target.blocks._blocks;
-                    if (!blocks) continue;
-
-                    for (const blockId in blocks) {
-                        if (!Object.prototype.hasOwnProperty.call(blocks, blockId)) continue;
-                        const block = blocks[blockId];
-                        if (!block) continue;
-                        if (
-                            block.opcode === 'procedures_prototype' ||
-                            block.opcode === this.ScratchBlocks.PROCEDURES_CALL_BLOCK_TYPE
-                        ) {
-                            procedureBlocks.push(block);
-                        }
-                    }
-                }
-                return procedureBlocks;
-            });
-        }
+        this.workspace.vm = this.props.vm;
         AddonHooks.blocklyWorkspace = this.workspace;
 
         // Register buttons under new callback keys for creating variables,
@@ -328,12 +302,6 @@ class Blocks extends React.Component {
     componentWillUnmount () {
         this.detachVM();
         this.unmounted = true;
-
-        if (this.ScratchBlocks && this.ScratchBlocks.Procedures &&
-            this.ScratchBlocks.Procedures.setProcedureBlocksAcrossTargetsCallback) {
-            this.ScratchBlocks.Procedures.setProcedureBlocksAcrossTargetsCallback(() => []);
-        }
-
         this.workspace.dispose();
         clearTimeout(this.toolboxUpdateTimeout);
 
@@ -551,29 +519,6 @@ class Blocks extends React.Component {
             }
             log.error(error);
         }
-
-        const allBlocks = this.workspace.getAllBlocks(false);
-        for (let i = 0; i < allBlocks.length; i++) {
-            const block = allBlocks[i];
-            if (block.type === this.ScratchBlocks.PROCEDURES_CALL_BLOCK_TYPE && block.getProcCode) {
-                const procCode = block.getProcCode();
-                if (procCode) {
-                    // Get the latest global procedure mutation from the provider
-                    const globalMutations = this.ScratchBlocks.Procedures.allGlobalProcedureMutations(this.workspace);
-                    for (let j = 0; j < globalMutations.length; j++) {
-                        const mutation = globalMutations[j];
-                        if (this.ScratchBlocks.Names.equals(mutation.getAttribute('proccode'), procCode)) {
-                            // Found the matching procedure, update the call block with the latest mutation
-                            if (block.domToMutation) {
-                                block.domToMutation(mutation);
-                            }
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
         this.workspace.addChangeListener(this.props.vm.blockListener);
 
         if (this.props.vm.editingTarget && this.props.workspaceMetrics.targets[this.props.vm.editingTarget.id]) {
@@ -723,8 +668,29 @@ class Blocks extends React.Component {
     handleCustomProceduresClose (data) {
         this.props.onRequestCloseCustomProcedures(data);
         const ws = this.workspace;
-        ws.refreshToolboxSelection_();
-        ws.toolbox_.scrollToCategoryById('myBlocks');
+        if (!ws) return;
+
+        const flyout = ws.getFlyout && ws.getFlyout();
+        if (flyout) {
+            flyout.setRecyclingEnabled(false);
+        }
+
+        const toolboxXML = this.getToolboxXML();
+        if (toolboxXML) {
+            this.props.updateToolboxState(toolboxXML);
+        }
+
+        this.requestToolboxUpdate();
+        this.withToolboxUpdates(() => {
+            const refreshedFlyout = ws.getFlyout && ws.getFlyout();
+            ws.refreshToolboxSelection_();
+            if (ws.toolbox_) {
+                ws.toolbox_.scrollToCategoryById('myBlocks');
+            }
+            if (refreshedFlyout) {
+                refreshedFlyout.setRecyclingEnabled(true);
+            }
+        });
     }
     handleDrop (dragInfo) {
         fetch(dragInfo.payload.bodyUrl)
