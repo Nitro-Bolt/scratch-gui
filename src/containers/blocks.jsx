25de3cfdfbaeea61c341bfb07710f26128275f38
Cubester <cubester.spamlegends@gmail.com>
2026-03-24 17:35:10 -0400

    Revert "Global Procedures (scratch-gui)"
    This reverts commit 6ed6be7a5774864040e473eae4685a52678dac81.

--- src/containers/blocks.jsx
+++ src/containers/blocks.jsx
@@ -158,9 +158,6 @@ class Blocks extends React.Component {
         const workspaceConfig = defaultsDeep({},
             this.props.options,
             {
-                vm: this.props.vm
-            },
-            {
                 rtl: this.props.isRtl,
                 toolbox: this.props.toolboxXML,
                 colours: this.props.theme.getBlockColors(),
@@ -171,7 +168,6 @@ class Blocks extends React.Component {
             Blocks.defaultOptions
         );
         this.workspace = this.ScratchBlocks.inject(this.blocks, workspaceConfig);
-        this.workspace.vm = this.props.vm;
         AddonHooks.blocklyWorkspace = this.workspace;
 
         // Register buttons under new callback keys for creating variables,
@@ -668,29 +664,8 @@ class Blocks extends React.Component {
     handleCustomProceduresClose (data) {
         this.props.onRequestCloseCustomProcedures(data);
         const ws = this.workspace;
-        if (!ws) return;
-
-        const flyout = ws.getFlyout && ws.getFlyout();
-        if (flyout) {
-            flyout.setRecyclingEnabled(false);
-        }
-
-        const toolboxXML = this.getToolboxXML();
-        if (toolboxXML) {
-            this.props.updateToolboxState(toolboxXML);
-        }
-
-        this.requestToolboxUpdate();
-        this.withToolboxUpdates(() => {
-            const refreshedFlyout = ws.getFlyout && ws.getFlyout();
-            ws.refreshToolboxSelection_();
-            if (ws.toolbox_) {
-                ws.toolbox_.scrollToCategoryById('myBlocks');
-            }
-            if (refreshedFlyout) {
-                refreshedFlyout.setRecyclingEnabled(true);
-            }
-        });
+        ws.refreshToolboxSelection_();
+        ws.toolbox_.scrollToCategoryById('myBlocks');
     }
     handleDrop (dragInfo) {
         fetch(dragInfo.payload.bodyUrl)
