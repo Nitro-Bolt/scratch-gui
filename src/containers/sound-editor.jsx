d99a2ab6f9d91963d2921c1804b4c45ce735633b
raynecloudy <hi@rayne.page>
2026-05-27 06:56:34 -1000

    better timestep options

--- src/containers/sound-editor.jsx
+++ src/containers/sound-editor.jsx
@@ -492,7 +492,7 @@ class SoundEditor extends React.Component {
     }
     setTimeSteps (duration) {
         if (!this.timeStepRef) return;
-        const stepOptions = [0.125, 0.25, 0.5, 1, 5, 10, 30, 60];
+        const stepOptions = [0.125, 0.25, 0.5, 1.5, 5, 15, 30, 60];
         const {width} = this.timeStepRef.getBoundingClientRect();
         let stepOption;
         for (let i = stepOptions.length - 1; i >= 0; i--) {
