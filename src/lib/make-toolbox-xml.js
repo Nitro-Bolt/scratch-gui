20fd26027b32bc733da47e3d0146631f40503870
ddededodediamante <lukitadiamante5@gmail.com>
2026-06-03 01:33:24 -0300

    switch case & extend merge obj/arr

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -484,7 +484,7 @@ const control = function (isInitialSetup, isStage, targetId, colors) {
         <block id="forever" type="control_forever"/>
         ${blockSeparator}
         <block type="control_if_extendable"/>
-        <block type="control_if_else_extendable"/>
+        <block type="control_switch"/>
         <block id="wait_until" type="control_wait_until"/>
         <block id="repeat_until" type="control_repeat_until"/>
         <block id="while" type="control_while"/>
@@ -813,7 +813,7 @@ const json = function (colors) {
                 </shadow>
             </value>
         </block>
-        <block type="json_merge_object">
+        <block type="json_merge_object_extendable">
         </block>
         <block type="json_has_key">
             <value name="KEY">
@@ -887,7 +887,7 @@ const json = function (colors) {
                 </shadow>
             </value>
         </block>
-        <block type="json_merge_array">
+        <block type="json_merge_array_extendable">
         </block>
         <block type="json_reverse_array">
         </block>
