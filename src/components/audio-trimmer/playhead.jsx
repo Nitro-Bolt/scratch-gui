2283a888bd2cf07fd55d91a0c82e3865aae9e6eb
raynecloudy <hi@rayne.page>
2026-07-15 02:58:49 -0700

    fix visual bug with playhead caret not changing with color themes

--- src/components/audio-trimmer/playhead.jsx
+++ src/components/audio-trimmer/playhead.jsx
@@ -2,7 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames';
 import styles from './audio-trimmer.css';
-import playheadCaret from './playhead-caret.svg';
+import TWRenderRecoloredImage from '../../lib/tw-recolor/render.jsx';
+import playheadCaret from '!../../lib/tw-recolor/build!./icon--playhead-caret.svg';
 
 const Playhead = props => (
     <>
@@ -20,7 +21,7 @@ const Playhead = props => (
                 transform: `translateX(${100 * props.playbackPosition}%)`
             }}
         >
-            <img
+            <TWRenderRecoloredImage
                 id="playhead-caret"
                 src={playheadCaret}
             />
