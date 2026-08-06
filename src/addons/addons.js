e698953158bcf853d152db304674d8ff50fd41dc
ddededodediamante <lukitadiamante5@gmail.com>
2026-08-06 01:32:00 -0300

    Stop stepping! I am very mad at you.

--- src/addons/addons.js
+++ src/addons/addons.js
@@ -72,8 +72,7 @@ const addons = [
     'tw-remove-feedback',
     'tw-disable-vibration',
     'tw-disable-cloud-variables',
-    'tw-disable-compiler',
-    'editor-stepping'
+    'tw-disable-compiler'
 ];
 
 const newAddons = [
