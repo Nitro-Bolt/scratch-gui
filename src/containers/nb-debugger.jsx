de23c82a71e91d087ac96ebdead5ef46e7d6a87a
raynecloudy <hi@rayne.page>
2026-09-01 23:50:23 -0700

    flash block chain

--- src/containers/nb-debugger.jsx
+++ src/containers/nb-debugger.jsx
@@ -16,6 +16,51 @@ import {
 } from '../reducers/debugger';
 import {activateTab, BLOCKS_TAB_INDEX} from '../reducers/editor-tab.js';
 
+/**
+ * Helper class to flash a Blockly scratch block chain in the users workspace
+ */
+class ThreadFlasher {
+  /**
+   * FLash a block 3 times
+   * @param block the block to flash
+   */
+  static flash(block) {
+    if (myFlash.timerID > 0) {
+      clearTimeout(myFlash.timerID);
+      if (myFlash.block.svgPath_) {
+        myFlash.block.svgPath_.style.fill = "";
+      }
+    }
+
+    let count = 4;
+    let flashOn = true;
+    myFlash.block = block;
+
+    /**
+     * Internal method to switch the colour of a block chain between light yellow and its original colour
+     * @private
+     */
+    function _flash() {
+      for (const block of myFlash.block.getDescendants()) {
+          if (block.svgPath_) {
+            block.svgPath_.style.fill = flashOn ? "#ffff80" : "";
+          }
+      }
+      flashOn = !flashOn;
+      count--;
+      if (count > 0) {
+        myFlash.timerID = setTimeout(_flash, 200);
+      } else {
+        myFlash.timerID = 0;
+        myFlash.block = null;
+      }
+    }
+
+    _flash();
+  }
+}
+
+const myFlash = { block: null, timerID: null };
 
 class NBDebugger extends React.Component {
     constructor (props) {
@@ -112,6 +157,7 @@ class NBDebugger extends React.Component {
         this.props.onActivateCodeTab();
         const workspace = window.ScratchBlocks.getMainWorkspace();
         workspace.centerOnBlock(block);
+        ThreadFlasher.flash(workspace.getBlockById(block));
     }
 
     componentDidUpdate (prevProps) {
